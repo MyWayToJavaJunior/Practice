a89846c38ea0593515ef1fe970f4f0e61524cb2b
Helen Kyryliuk <helen.kyryliuk@gmail.com>
2017-11-14 00:51:25 +0200

    changed

--- Math.js
+++ Math.js
@@ -1,9 +1,12 @@
 // BEGIN (write your solution here)
 const transform = (n) => {
-    return n * Math.PI / 180;
+    return (n * Math.PI) / 180;
   };
-  export default (a, b, c) => {
-    return ((a * a) * (1 / 2)) * (Math.sin(transform(b)) * Math.sin(transform(c)) / Math.sin(transform(180) - transform(b + c)));
+  export default (side, angle1, angle2) => {
+    const sin1 = Math.sin(transform(angle1));
+    const sin2 = Math.sin(transform(angle2));
+    const sin3 = Math.sin(transform(180 - (angle1 + angle2)));
+    return (Math.pow(side, 2) / 2) * ((sin1 * sin2) / sin3);
   };
   // END
   
