1a2a4fc31b52d3b788fa828ce9cc41e997246a8a
Helen Kyryliuk <helen.kyryliuk@gmail.com>
2017-11-14 01:50:13 +0200

    game check

---
+++ If.js
@@ -0,0 +1,18 @@
+// BEGIN (write your solution here)
+export default (a, b, c, d) => {
+    if (a === c && b === d) {
+      return 2;
+    }
+    else if ((a === b) && (c === d)) {
+      return 1;
+    }
+    else if (a > b && c > d) {
+      return 1;
+    }
+    else if (a < b && c < d) {
+      return 1;
+    }
+    else return 0;
+    
+  };
+  // END
