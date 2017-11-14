941952f76e0ec83eed851fbb0bbc36aa10e61f2b
Helen Kyryliuk <helen.kyryliuk@gmail.com>
2017-11-14 21:07:10 +0200

    assert.equal

---
+++ Tests.js
@@ -0,0 +1,9 @@
+// Example
+assert.equal(factorial(2), 2);
+assert.equal(factorial(3), 6);
+
+// BEGIN (write your solution here)
+assert.equal(factorial(1), 1);
+assert.equal(factorial(0), 1);
+assert.equal(factorial(4), 24);
+// END 
