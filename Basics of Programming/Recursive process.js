46d83696b208f4bf9256f06ffadb9d5fc90fddbf
Helen Kyryliuk <helen.kyryliuk@gmail.com>
2017-11-19 22:38:14 +0200

    recursive

---
+++ Basics of Programming/Recursive process.js
@@ -0,0 +1,12 @@
+// BEGIN (write your solution here)
+ const sequenceSum = (begin, end) => {
+    if (begin > end) {
+      return NaN;
+    }
+    if (begin === end) {
+      return begin;
+    }
+    return begin + sequenceSum(begin + 1, end);
+};
+  
+  // END
