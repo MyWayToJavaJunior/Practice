d0fa6bf6a611afb32dd9e9d22292f13fe6d32a20
Helen Kyryliuk <helen.kyryliuk@gmail.com>
2017-11-24 03:00:23 +0200

    segm

---
+++ Composite Data/rectangles.js
@@ -0,0 +1,24 @@
+// BEGIN (write your solution here)
+export const makeRectangle = (startPoint, width, height) => {
+    
+    };
+    export const startPoint = (makeRectangle) => {
+      return 
+    };
+    export const width = (makeRectangle) => {
+      return
+    };
+    export const height = (makeRectangle) => {
+    
+    };
+    export const square = () => {
+    
+    };
+    export const perimeter = () => {
+    
+    };
+    export const containsTheOrigin = () => {
+    
+    };
+    // END
+    
