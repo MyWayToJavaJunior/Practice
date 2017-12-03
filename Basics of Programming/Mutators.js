0cce454906b4a86462e1a3e49e44ef8a06d4cb73
Helen Kyryliuk <helen.kyryliuk@gmail.com>
2017-12-03 18:34:51 +0200

    mutators

---
+++ Basics of Programming/Mutators.js
@@ -0,0 +1,25 @@
+const isPrime1 = (num) => {
+    if (num < 2) {
+      return false;
+    }
+    let i = 2;
+    while (i <= num / 2) {
+      if (num % i === 0) {
+        return false;
+      }
+      i += 1; // вместо i++;
+    }
+  
+    return true;
+  };
+  export default (num) => {
+    if (num <= 1) {
+      return false; 
+    }
+    let result = 2;
+    while (num % result !== 0) {
+      result = result + 1;
+    }
+    return result === num;
+  };
+  
---
+++ Basics of Programming/grandCommonDivisor.js
@@ -0,0 +1,9 @@
+const gcd = (a, b) => {
+    if (b === 0) {
+      return a;
+    }
+  
+    return gcd(b, a % b);
+  };
+  
+  export default gcd;
