47ba7b5208466b48469c3e2f983ed8eb881ef022
Helen Kyryliuk <helen.kyryliuk@gmail.com>
2017-12-20 00:38:03 +0200

    recursion reverse

--- Composite Data/findPrimitiveBox.js
+++ Composite Data/findPrimitiveBox.js
@@ -10,5 +10,15 @@ const findPrimitiveBox = (pair) => {
        return findPrimitiveBox(car(pair));
     }
   };
+  const findPrimitiveBox = (pair) => {
+    const first = car(pair);
+    const last = cdr(pair);
   
+    if (!isPair(first) && !isPair(last)) {
+      return pair;
+    }
+  
+    const next = isPair(first) ? first : last;
+    return findPrimitiveBox(next);
+  };
   // END
