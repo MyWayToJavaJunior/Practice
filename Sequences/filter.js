d1bdbd28a2ab0cae86080dc44100e14515a58c8f
Helen Kyryliuk <helen.kyryliuk@gmail.com>
2017-12-26 15:55:17 +0200

    filter

---
+++ Sequences/filter.js
@@ -0,0 +1,20 @@
+// BEGIN
+export const filter = (func, elements) => {
+    const iter = (items, acc) => {
+      if (isEmpty(items)) {
+        return reverse(acc);
+      }
+      const item = head(items);
+      const newAcc = func(item) ? cons(item, acc) : acc;
+      return iter(tail(items), newAcc);
+    };
+  
+    return iter(elements, l());
+  };
+  
+  export const quotes = (elements) => {
+    const filtered = filter(element => is('blockquote', element), elements);
+    const result = map(value, filtered);
+    return result;
+  };
+  // END
