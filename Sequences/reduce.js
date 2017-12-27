a9696d9e3ee11add1bedbc46d3496285730b3f2b
Helen Kyryliuk <helen.kyryliuk@gmail.com>
2017-12-27 02:12:40 +0200

    reduce

---
+++ Sequences/reduce.js
@@ -0,0 +1,15 @@
+// BEGIN
+export const reduce = (func, acc, elements) => {
+    if (isEmpty(elements)) {
+      return acc;
+    }
+  
+    return reduce(func, func(head(elements), acc), tail(elements));
+  };
+  
+  export const emptyTagsCount = (tagName, elements) => {
+    const predicate = element => is(tagName, element) && value(element) === '';
+    const func = (element, acc) => (predicate(element) ? acc + 1 : acc);
+    return reduce(func, 0, elements);
+  };
+  // END
